9ce769956b45581d5e478726f0ee9e7b678bbf27
Douglas Barbosa <douglas.makubex@gmail.com>
2017-04-13 22:51:14 -0300

    commit

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelavent variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/douglas/projects/djg/myenv"
+set -gx VIRTUAL_ENV "/home/douglas/projects/djg/myfirstblog/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
