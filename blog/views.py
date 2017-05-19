e2764039380b949cfb85bc33eff9b9e27be5cc79
Douglas Barbosa <douglas.makubex@gmail.com>
2017-05-19 13:09:57 -0300

    Adicionado paginação na listagem inicial dos posts

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts, 3)
+    try:
+        posts_paginados = paginator.page(page)
+    except PageNotAnInteger:
+        posts_paginados = paginator.page(1)
+    except EmptyPage:
+        posts_paginados = paginator.page(paginator.num_pages)
+
+    
+
+    return render(request, 'blog/post_list.html', {'posts_paginados':posts_paginados})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post':post})
+    return render(request, 'blog/post_detail.html', {'posts_paginados':posts_paginados})
 
 @login_required
 def post_new(request):
