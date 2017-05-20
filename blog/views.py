cfebbd2d29261be31fe20b662457800dba0ffed3
Douglas Barbosa <douglas.makubex@gmail.com>
2017-05-19 22:59:23 -0300

    Corrigido bug de paginação

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+#def post_list(request):
+#    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+#    return render(request, 'blog/post_list.html', {'posts':posts})
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+    queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -60,3 +72,5 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+post_list = PostList.as_view()
