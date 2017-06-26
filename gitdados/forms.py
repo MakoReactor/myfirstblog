ac329ceac817d17af848f7c115b15342775352ac
Douglas Barbosa <douglas.makubex@gmail.com>
2017-06-26 18:13:41 -0300

    adicionada pagina cep

--- gitdados/forms.py
+++ gitdados/forms.py
@@ -1,13 +1,8 @@
 from django import forms
 
-from .models import MeuCep
-
 
 class MeuCepForm(forms.Form):
-    consulta_cep = forms.CharField(widget.TextInput(attrs={'class':'form-control', 'placeholder':'Digite um cep. Apenas Numeros.'}))
+    consulta_cep = forms.CharField(widget = forms.TextInput(attrs={'class':'form-control', 'placeholder':'Digite um cep. Apenas Numeros.'}))
+    
 
 
-
-
-class GitDadosForm(forms.Form):
-    git_name = forms.CharField(label="Git name", max_length=200)
--- gitdados/views.py
+++ gitdados/views.py
@@ -3,35 +3,44 @@ from django.shortcuts import render, get_object_or_404, redirect
 #from django.utils import timezone
 import requests
 # Form import
-from .forms import GitDadosForm, MeuCepForm
+from .forms import MeuCepForm
 
 def mycep(request):
     template_name = 'gitdados/mycep.html'
-    context = {}
+    context = {}  
 
-    if request.method = "POST":
+    if request.method == "POST":
         form = MeuCepForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            
-
-
+            consulta = request.POST.get('consulta_cep')
+            cep_data = requests.get('https://viacep.com.br/ws/{0}/json/'.format(consulta)).json()
+            context = {
+                'form':form, 'cep_data':cep_data
+                }
+            return render (request, template_name, context)
+    else:
+        form = MeuCepForm()
+        context = {
+        'form':form
+        }
+        
     return render(request, template_name, context)
+
+
 
 def marketing(request):
     template_name = 'gitdados/marketing.html'
     context = {}
     return render(request, template_name, context)
 
+
 def profile(request):
     template_name = 'gitdados/profile.html'
     context = {}
-
     if request.method == 'POST':
         username = request.POST.get('user')
         req = requests.get('https://api.github.com/users/{}'.format(username)).json()
         context['req']=req
-
     return render(request, template_name, context)
 
 
